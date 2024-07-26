3b40ddd3fc4fb9ee0fa76c46ae59b3fd5fae6016
Swapnil Narwade <swapnil.narwade3@gmail.com>
2024-07-27 03:10:07 +0530

    updated auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -10,20 +10,10 @@ export const register = async (req: Request, res: Response) => {
     if (!username || !email || !!password) {
       res.status(400).json({ message: "Invalid Inputs" });
     }
-    const user = await userClient.create({
-      data: {
-        username: username,
-        email: email,
-        password: password,
-      },
-    });
-    res.status(201).json({
-      message: "User created sucessfully",
-      link: `../api/v1/users/${user.id}`,
-    });
+    const user = await userClient.create({ data: { username: username, email: email, password: password }});
+    res.status(201).json({ message: "User created sucessfully", link: `../api/v1/users/${user.id}` });
   } catch (e) {
-    console.log(e);
-    res.status(500).json({ message: "Internal Server Error!" });
+    res.status(500).json({ error: "Internal Server Error!" });
   }
 };
 
@@ -35,16 +25,14 @@ export const deleteUser = async (req: Request, res: Response) => {
     } else if (password !== confirmpass) {
       res.status(400).json({ message: "Password does not match!" });
     } else {
-      const userToBeDeleted = await userClient.delete({
-        where: {
-          email: email,
-        },
-      });
+      const userToBeDeleted = await userClient.delete({ where: { email: email }});
       if (!userToBeDeleted) {
         res.status(404).json({ message: "User not found!" });
       }
       res.status(200).json({ message: "User deleted successfully" });
     }
     res.status(400).json({ message: "Enter correct inputs!" });
-  } catch (e) {}
+  } catch (e) {
+    res.status(500).json({ error: "Internal Server Error!" });
+  }
 };
